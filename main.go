1c7051be531edc6e90625f0b3aec58479a69668f
KingMicky <dikachi.ogalabu@gmail.com>
2024-07-15 11:31:20 +0100

    chore: Add dependencies and routes for order handling

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/kingmicky/micro-go/application"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	app := application.New()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
+	if err != nil {
+		fmt.Println("Failed to start app:", err)
+	}
+}
